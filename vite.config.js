fa5d0a57cc6f0ed35a51bf7e092f827c3655e658
PatrickSkrebel <peskrebel@email.neit.edu>
2024-12-14 16:51:46 -0500

    refresh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Change this to '/app/' if hosted in a subdirectory
   server: {
     proxy: {
       '/api': {
